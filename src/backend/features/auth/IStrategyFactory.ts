38fd970a956df2c61a45caa734295c2e0e3be590
Marwan-Mamdouh <marwanabdalmagied@gmail.com>
2026-08-02 00:40:13 +0300

    chore: clean up unused files, remove dead code, and fix types

--- src/backend/features/auth/IStrategyFactory.ts
+++
@@ -1,3 +0,0 @@
-export interface IStrategyFactory<T> {
-	getStrategy(userType: string): T;
-}
--- src/types/cart.ts
+++
@@ -1,8 +0,0 @@
-export type CartItems = {
-	id: string;
-	title: string;
-	description?: string;
-	image: string;
-	price: number; // cents or decimal? prefer cents (int) to avoid float errors
-	quantity: number;
-};
